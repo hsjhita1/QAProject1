198e73425eaae0d6cca621a2e6590af598850c8f
hsjhita1 <hjhita1997@gmail.com>
2020-05-11 16:16:25 +0100

    Upload of Flask project with working database connection. add_data.py allows users to input data into the database from a CLI

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,18 @@
 from flask import render_template
 from application import app
+from application.models import Posts
 
 @app.route('/')
 @app.route('/home')
 
 def home():
-    
-    return render_template('home.html', title='Home Page')
+    postData = Posts.query.first()
+    return render_template('home.html', title='Home Page', post=postData)
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login Page')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register Page')
