5ef05a737c4b0b623db85b289fdd871a9cffc4b5
hsjhita1 <hjhita1997@gmail.com>
2020-05-20 16:41:36 +0000

    Fixed typos and errors in initial test file

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -1,4 +1,4 @@
-import uniitest
+import unittest
 
 from flask import url_for
 from flask_testing import TestCase
@@ -28,9 +28,9 @@ class TestBase(TestCase):
         hashed_pw_1 = bcrypt.generate_password_hash('admin2020')
         admin = Users(
             user_name = "admin",
-            first_name = "admin"
-            last_name = "admin"
-            email = "admin@test.com"
+            first_name = "admin",
+            last_name = "admin",
+            email = "admin@test.com",
             password = hashed_pw_1
             )
 
@@ -45,8 +45,8 @@ class TestViews(TestBase):
     def test_homepage_view(self):
         # Check if home is accessible without login
         homeResponse = self.client.get(url_for('home'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(homeResponse.status_code, 200)
 
         # Check if games is accessible without login
         gameResponse = self.client.get(url_for('games'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(gameResponse.status_code, 200)
