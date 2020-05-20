24be61240b0e13e22dc77d2754f34583a93556d2
hsjhita1 <hjhita1997@gmail.com>
2020-05-20 10:44:47 +0000

    updated installtion script

--- script/installation.sh
+++ script/installation.sh
@@ -10,7 +10,7 @@ sudo apt install python3-venv -y
 
 python3 -m venv venv
 
-source /var/lib/jenkins/workspace/flask-project/venv/bin/activate
+. /var/lib/jenkins/workspace/flask-project/venv/bin/activate
 
 pip3 install -r requirements.txt
 
