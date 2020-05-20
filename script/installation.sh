3b57aae78eb758c804727f89e1d0094f30805799
hsjhita1 <hjhita1997@gmail.com>
2020-05-20 13:33:03 +0000

    installation.sh test

--- script/installation.sh
+++ script/installation.sh
@@ -14,5 +14,4 @@ source /var/lib/jenkins/workspace/flask-project/venv/bin/activate
 
 pip3 install -r requirements.txt
 
-gunicorn --workers=4 --bind=0.0.0.0:5000 app:app
-#python3 /var/lib/jenkins/workspace/flask-project/app.py
+python3 /var/lib/jenkins/workspace/flask-project/app.py
